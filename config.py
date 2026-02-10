43bfb31aaf4b5f83fd2580271ffeec1b80d51bcc
Klubnijkeee <35773195+Klubnijkeee@users.noreply.github.com>
2026-02-10 15:03:03 +0300

    Change CHECK_INTERVAL to read from environment variable

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
 MIN_PROFIT = float(os.getenv('MIN_PROFIT', 0.8))
-CHECK_INTERVAL = 30  # секунд
+CHECK_INTERVAL = int(os.getenv('CHECK_INTERVAL', 30))
